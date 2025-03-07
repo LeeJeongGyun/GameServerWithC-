ed443a86ef7768330befa91bccf81ab56d1ef62e
LeeJeongGyun <jeonggyun0630@gmail.com>
2025-03-07 20:34:53 +0900

    Reader-Writer Lock 구현

--- Server/ServerCore/CoreMacro.h
+++ Server/ServerCore/CoreMacro.h
@@ -1,17 +1,31 @@
 #pragma once
 
-#define CRASH(cause) \
-{                           \
-    uint32 *ptr = nullptr;  \
-    __analysis_assume(ptr != nullptr); \
-    *ptr = 0xAAAAAAAA;      \
+#define OUT
+
+// Lock Macro
+#define USE_MANY_LOCKS(count)   Lock _locks[count]
+#define USE_LOCK                USE_MANY_LOCKS(1)
+#define READ_LOCK_IDX(idx)      ReadLockGuard readLockGuard_##idx(_locks[idx])
+#define READ_LOCK               READ_LOCK_IDX(0)
+#define WRITE_LOCK_IDX(idx)     WriteLockGuard writeLockGuard_##idx(_locks[idx])
+#define WRITE_LOCK              WRITE_LOCK_IDX(0)
+
+
+// CRASH
+
+#define CRASH(cause)                    \
+{                                       \
+    uint32 *ptr = nullptr;              \
+    __analysis_assume(ptr != nullptr);  \
+    *ptr = 0xAAAAAAAA;                  \
 }
 
-#define ASSERT_CRASH(expr)      \
-{                               \
-    if (!(expr))                \
-    {                           \
-        CRASH("ASSERT_CRASH");  \
-        __analysis_assume(expr);    \
-    }                           \
+#define ASSERT_CRASH(expr)              \
+{                                       \
+    if (!(expr))                        \
+    {                                   \
+        CRASH("ASSERT_CRASH");          \
+        __analysis_assume(expr);        \
+    }                                   \
 }
+
---
+++ Server/ServerCore/Lock.cpp
@@ -0,0 +1,81 @@
+#include "pch.h"
+#include "Lock.h"
+#include "CoreTLS.h"
+
+void Lock::WriteLock()
+{
+    uint32 curThreadId = (_lockFlag & WRITE_THREAD_MASK) >> 16;
+    if (curThreadId == LThreadId)
+    {
+        _writeCount++;
+        return;
+    }
+
+    const uint64 beginTick = ::GetTickCount64();
+    while (true)
+    {
+        uint32 desired = (LThreadId << 16) & WRITE_THREAD_MASK;
+        for (int32 spinCount = 0; spinCount < MAX_SPIN_COUNT; ++spinCount)
+        {
+            uint32 expected = EMPTY_FLAG;
+            if (_lockFlag.compare_exchange_strong(OUT expected, desired))
+            {
+                _writeCount++;
+                return;
+            }
+        }
+
+        if (::GetTickCount64() - beginTick > ACQUIRE_TIMEOUT_TICK)
+            CRASH("LOCK TIMEOUT");
+
+        _mm_pause(); // YieldProcessor();
+    }
+}
+
+void Lock::WriteUnlock()
+{
+    uint32 curThreadId = (_lockFlag & WRITE_THREAD_MASK) >> 16;
+    if (curThreadId != LThreadId)
+        CRASH("INVALID WRITE UNLOCK");
+
+    // R Lock 전부 풀기전에 W Lock 불가능
+    if ((_lockFlag & READ_COUNT_MASK) != 0)
+        CRASH("INVALID UNLOCK ORDER");
+
+    if (--_writeCount != 0)
+        return;
+
+    _lockFlag = EMPTY_FLAG;
+}
+
+void Lock::ReadLock()
+{
+    uint32 curThreadId = (_lockFlag & WRITE_THREAD_MASK) >> 16;
+    if (curThreadId == LThreadId)
+    {
+        _lockFlag.fetch_add(1);
+        return;
+    }
+
+    const uint64 beginTick = ::GetTickCount64();
+    while (true)
+    {
+        for (int spinCount = 0; spinCount < MAX_SPIN_COUNT; ++spinCount)
+        {
+            uint32 expected = _lockFlag & READ_COUNT_MASK;
+            if (_lockFlag.compare_exchange_strong(OUT expected, expected + 1))
+                return;
+        }
+
+        if (::GetTickCount64() - beginTick > ACQUIRE_TIMEOUT_TICK)
+            CRASH("LOCK TIMEOUT");
+
+        _mm_pause();
+    }
+}
+
+void Lock::ReadUnlock()
+{
+    if (0 == (_lockFlag.fetch_sub(1) & READ_COUNT_MASK))
+        CRASH("INVALID READ UNLOCK");
+}
